e1496481466ede417f0b33ba9d0c43b16626b519
Mm12345Mm12345 <133651646+Mm12345Mm12345@users.noreply.github.com>
2026-02-09 10:48:42 +0200

    Add files via upload

---
+++ robocup-rescue-2026-newboty_2.0.ino
@@ -0,0 +1,111 @@
+
+
+// pins
+
+#define ena 2
+#define in1 3
+#define in2 4
+#define in3 5
+#define in4 6
+#define enb 7
+
+#define rightSensor 26
+#define centerSensor 27
+#define leftSensor 28
+
+
+#define speed 230
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ena,OUTPUT);
+pinMode(in1,OUTPUT);
+pinMode(in2,OUTPUT);
+pinMode(in3,OUTPUT);
+pinMode(in4,OUTPUT);
+pinMode(enb,OUTPUT);
+
+pinMode(leftSensor,INPUT);
+pinMode(centerSensor,INPUT);
+pinMode(rightSensor,INPUT);
+
+Serial.begin(9600);
+
+}
+
+void forward() {
+ digitalWrite(in1,HIGH);
+digitalWrite(in2,LOW);
+analogWrite(ena,speed);
+digitalWrite(in3,HIGH);
+digitalWrite(in4,LOW);
+analogWrite(enb,speed);
+}
+
+void right() {
+ digitalWrite(in1,HIGH);
+digitalWrite(in2,LOW);
+analogWrite(ena,speed);
+digitalWrite(in3,HIGH);
+digitalWrite(in4,LOW);
+analogWrite(enb,0);
+}
+
+void left() {
+   digitalWrite(in1,HIGH);
+digitalWrite(in2,LOW);
+analogWrite(ena,0);
+digitalWrite(in3,HIGH);
+digitalWrite(in4,LOW);
+analogWrite(enb,speed);
+}
+void stop() {
+   digitalWrite(in1,HIGH);
+digitalWrite(in2,LOW);
+analogWrite(ena,0);
+digitalWrite(in3,HIGH);
+digitalWrite(in4,LOW);
+analogWrite(enb,0);
+}
+
+void loop() {
+  Serial.print(digitalRead(leftSensor));
+  Serial.print("\t");
+  Serial.print(digitalRead(centerSensor));
+  Serial.print("\t");
+  Serial.println(digitalRead(rightSensor));
+  
+  
+  if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==HIGH)
+   {
+   forward();
+  }
+  else if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==HIGH)
+   {
+   left();
+  }
+  else if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==LOW)
+   {
+   right();
+  }
+
+  else{
+    forward();
+  }
+
+/*
+  if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==LOW)
+   {
+  forward();
+  }
+  if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==LOW)
+  {
+   left();
+  }
+  if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==HIGH) 
+  {
+    right();
+}
+*/
+}
