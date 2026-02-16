cb8539a63ff42d12e700a7648dfd46c22e6ee70c
Dr-zalbone <dr.zalbone@gmail.com>
2026-02-16 13:58:01 +0200

    some corrections

--- code/code.ino
+++ code/code.ino
@@ -6,16 +6,21 @@
 #define in4 6
 #define enb 7
 
+#define LEFT true
+#define RIGHT false
+
 #define leftSensor 28
 #define centerSensor 27
 #define rightSensor 26
 
-const int speed = 225;
+const int speed = 200;
+
+bool current_turn = LEFT;
 
 long long current_millis = 0;
 long long start_millis = 0;
 
-const int forward_check_time = 1000;
+const int forward_check_time = 500;
 
 void setup() {
   // put your setup code here, to run once:
@@ -38,8 +43,11 @@ void forward_a_bit(){
   current_millis = millis();
 
   while(current_millis - start_millis < forward_check_time){
+    current_millis = millis();
     forward();
   }
+
+  current_turn ? left() : right();
 }
 
 void forward() {
@@ -84,6 +92,10 @@ void loop() {
   Serial.print("\t");
   Serial.println(digitalRead(rightSensor));
 
+
+  if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==HIGH){
+    forward_a_bit();
+  }
   
   if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==HIGH)
   {
@@ -92,27 +104,29 @@ void loop() {
   else if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==HIGH)
   {
     left();
+    current_turn = LEFT;
   }
   else if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==LOW)
   {
     right();
+    current_turn = RIGHT;
   }
-  if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==HIGH)
-  {
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-    analogWrite(ena,200);
-    digitalWrite(in3,HIGH);
-    digitalWrite(in4,LOW);
-    analogWrite(enb,200);
-  }
-  if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==LOW)
-  {
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
-    analogWrite(ena,200);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,HIGH);
-    analogWrite(enb,200);
-  }
+  // if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==HIGH)
+  // {
+  //   digitalWrite(in1,LOW);
+  //   digitalWrite(in2,HIGH);
+  //   analogWrite(ena,200);
+  //   digitalWrite(in3,HIGH);
+  //   digitalWrite(in4,LOW);
+  //   analogWrite(enb,200);
+  // }
+  // if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==LOW)
+  // {
+  //   digitalWrite(in1,HIGH);
+  //   digitalWrite(in2,LOW);
+  //   analogWrite(ena,200);
+  //   digitalWrite(in3,LOW);
+  //   digitalWrite(in4,HIGH);
+  //   analogWrite(enb,200);
+  // }
 }
