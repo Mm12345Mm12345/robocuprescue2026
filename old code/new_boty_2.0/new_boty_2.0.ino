2f2f3a0f7afc2a88b053ccf2d62a7492e355c304
Mm12345Mm12345 <133651646+Mm12345Mm12345@users.noreply.github.com>
2026-02-16 11:36:30 +0200

    start robot know distence

--- old code/new_boty_2.0/new_boty_2.0.ino
+++ old code/new_boty_2.0/new_boty_2.0.ino
@@ -1,3 +1,8 @@
+#include <NewPing.h>
+  int Speed = 65;
+  int SlowSpeed = 0;
+  int turnSpeed = 55;
+
 // pins
 #define ena 2
 #define in1 3
@@ -10,7 +15,19 @@
 #define centerSensor 27
 #define rightSensor 26
 
+#define front_trigPin 22
+#define front_echoPin 23
+  long duration;
+  long distance;
+int numberOfSamples = 10, counter=0;
+long sum=0; 
 
+/*#define SIDE_TRIGGER_PIN 22 // Arduino pin tied to trigger pin on the ultrasonic sensor.
+  #define SIDE_ECHO_PIN 23 // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+  #define FRONT_TRIGGER_PIN 33 // Arduino pin tied to trigger pin on the ultrasonic sensor.
+  #define FRONT_ECHO_PIN 32 // Arduino pin tied to echo pin on the ultrasonic sensor.
+  #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.*/*
 void setup() {
   // put your setup code here, to run once:
 pinMode(ena,OUTPUT);
@@ -26,10 +43,14 @@ pinMode(rightSensor,INPUT);
 
 Serial.begin(9600);
 
+pinMode(front_trigPin, OUTPUT);
+pinMode(front_echoPin, INPUT);
+Serial.begin(9600);
 }
 
-void forward() {
- digitalWrite(in1,HIGH);
+void forward  () 
+{
+  digitalWrite(in1,HIGH);
 digitalWrite(in2,LOW);
 analogWrite(ena,200);
 digitalWrite(in3,HIGH);
@@ -100,11 +121,58 @@ void loop() {
        digitalWrite(in3,LOW);
        digitalWrite(in4,HIGH);
        analogWrite(enb,200);
+
+
+  if (distance <5)
+  {
+    
+  }
     }
   else{
    // stop();
+
+ while(counter <numberOfSamples){
+    
+  getDistance();
+    
+    if(distance>3 && distance <300){
+    sum+=distance;
+      counter++;
+    }
   }
+    
+  Serial.print("average distance[cm] = ");
+  Serial.println(sum*1.0/counter);
+  delay(10);
+  counter = 0;
+  sum=0;
+  }
+
+void getDistance(){
+  digitalWrite(front_trigPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(front_trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(front_trigPin, LOW);
+  
+  duration = pulseIn(front_echoPin,HIGH);
+  distance = duration/58.1;
+  
+}
+
+ // לא מזהה קו לפנות 90 מעלות
+    while (digitalRead(sens3) != LOW) { //כל עוד החיישן האמצעי לא מוצא קו תסתובב 90 מעלות
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+      leftSpeed = turnSpeed;
+      rightSpeed = turnSpeed;
+      analogWrite(ena, leftSpeed);
+      analogWrite(enb, rightSpeed);
+    }
 /*
+
 
   if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==LOW)
    {
