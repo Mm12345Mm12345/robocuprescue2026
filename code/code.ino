c2bee6f8eca8c1aa0b55baf25f609fa034933a11
Dr-zalbone <dr.zalbone@gmail.com>
2026-03-05 09:22:45 +0200

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -10,113 +10,96 @@
 #define centerSensor 27
 #define rightSensor 26
 
+int leftInput;
+int centerInput;
+int rightInput;
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(ena,OUTPUT);
-pinMode(in1,OUTPUT);
-pinMode(in2,OUTPUT);
-pinMode(in3,OUTPUT);
-pinMode(in4,OUTPUT);
-pinMode(enb,OUTPUT);
+  pinMode(ena,OUTPUT);
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(enb,OUTPUT);
 
-pinMode(leftSensor,INPUT);
-pinMode(centerSensor,INPUT);
-pinMode(rightSensor,INPUT);
+  pinMode(leftSensor,INPUT);
+  pinMode(centerSensor,INPUT);
+  pinMode(rightSensor,INPUT);
 
-Serial.begin(9600);
-
+  Serial.begin(9600);
 }
-// this is forward
 void forward() {
- digitalWrite(in1,HIGH);
-digitalWrite(in2,LOW);
-analogWrite(ena,200);
-digitalWrite(in3,HIGH);
-digitalWrite(in4,LOW);
-analogWrite(enb,200);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  analogWrite(ena,200);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  analogWrite(enb,200);
 }
-
 void right() {
- digitalWrite(in1,LOW);
-digitalWrite(in2,LOW);
-analogWrite(ena,200);
-digitalWrite(in3,HIGH);
-digitalWrite(in4,LOW);
-analogWrite(enb,200);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  analogWrite(ena,200);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  analogWrite(enb,200);
 }
-
 void left() {
- digitalWrite(in1,HIGH);
-digitalWrite(in2,LOW);
-analogWrite(ena,200);
-digitalWrite(in3,LOW);
-digitalWrite(in4,LOW);
-analogWrite(enb,200);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  analogWrite(ena,200);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+  analogWrite(enb,200);
 }
 void stop() {
-   digitalWrite(in1,HIGH);
-digitalWrite(in2,LOW);
-analogWrite(ena,0);
-digitalWrite(in3,HIGH);
-digitalWrite(in4,LOW);
-analogWrite(enb,0);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  analogWrite(ena,0);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  analogWrite(enb,0);
 }
 
 void loop() {
-  Serial.print(digitalRead(leftSensor));
-  Serial.print("\t");
-  Serial.print(digitalRead(centerSensor));
-  Serial.print("\t");
-  Serial.println(digitalRead(rightSensor));
+  leftInput = digitalRead(leftSensor); // digital read returns true if it doesn't detect a line, and false if it does
+  centerInput = digitalRead(centerSensor);
+  rightInput = digitalRead(rightSensor);
 
-  
-  if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==HIGH)
-   {
-  // forward();
-  }
-  else if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==HIGH)
-   {
-   left();
-  }
-  else if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==LOW)
-   {
-   right();
-  }
-  if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==HIGH)
-    {
-       digitalWrite(in1,LOW);
-       digitalWrite(in2,HIGH);
-       analogWrite(ena,200);
-       digitalWrite(in3,HIGH);
-       digitalWrite(in4,LOW);
-       analogWrite(enb,200);
-    }
-  if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==LOW)
-    {
-       digitalWrite(in1,HIGH);
-       digitalWrite(in2,LOW);
-       analogWrite(ena,200);
-       digitalWrite(in3,LOW);
-       digitalWrite(in4,HIGH);
-       analogWrite(enb,200);
-    }
-  else{
-   // stop();
-  }
-/*
+  Serial.print(leftInput);
+  Serial.print("\t");
+  Serial.print(centerInput);
+  Serial.print("\t");
+  Serial.println(rightInput);
 
-  if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==HIGH && digitalRead(rightSensor)==LOW)
-   {
-  forward();
-  }
-  if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==LOW)
+  // if (leftInput && !centerInput && rightInput)
+  // {
+  //   forward();
+  // }
+  if (!leftInput && centerInput && rightInput)
   {
-   left();
+    left();
   }
-  if (digitalRead(leftSensor)==LOW && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==HIGH) 
+  else if (leftInput && centerInput && !rightInput)
   {
     right();
-}
-*/
+  }
+  if (!leftInput && !centerInput && rightInput)
+  {
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+    analogWrite(ena,200);
+    digitalWrite(in3,HIGH);
+    digitalWrite(in4,LOW);
+    analogWrite(enb,200);
+  }
+  if (leftInput && !centerInput && !rightInput)
+  {
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    analogWrite(ena,200);
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+    analogWrite(enb,200);
+  }
 }
