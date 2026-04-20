040ba7cf1328540c70128488612314d3e5346f8c
Dr-zalbone <dr.zalbone@gmail.com>
2026-04-20 09:22:01 +0300

    color sensor alpha

--- code/code.ino
+++ code/code.ino
@@ -22,8 +22,51 @@
 // NewPing front_sonar(front_trigPin, front_echoPin, MAX_DISTANCE);
 // NewPing side_sonar(front_echoPin, FRONT_ECHO_PIN, MAX_DISTANCE);
 
+#define LEFT_S0 46
+#define LEFT_S1 47
+#define LEFT_S2 48
+#define LEFT_S3 49
+#define LEFT_sensorOut 22
 
-const int speed = 200
+#define RIGHT_S0 50
+#define RIGHT_S1 51
+#define RIGHT_S2 52
+#define RIGHT_S3 53
+#define RIGHT_sensorOut 23
+
+#define RIGHT false
+#define LEFT true
+
+// Variables to store frequency
+int redFrequency = 0;
+int greenFrequency = 0;
+
+int getGreen(bool direction){
+  digitalWrite(direction ? LEFT_S2 : RIGHT_S2, HIGH);
+  digitalWrite(direction ? LEFT_S3 : RIGHT_S3, HIGH);
+  int greenFreq = pulseIn(direction ? LEFT_sensorOut : RIGHT_sensorOut, LOW);
+  return greenFreq;
+}
+
+int getRed(bool direction){
+  digitalWrite(direction ? LEFT_S2 : RIGHT_S2, LOW);
+  digitalWrite(direction ? LEFT_S3 : RIGHT_S3, LOW);
+  int redFreq = pulseIn(direction ? LEFT_sensorOut : RIGHT_sensorOut, LOW);
+  return redFreq;
+}
+
+int getBlue(bool direction){
+  digitalWrite(direction ? LEFT_S2 : RIGHT_S2, LOW);
+  digitalWrite(direction ? LEFT_S3 : RIGHT_S3, HIGH);
+  int blueFreq = pulseIn(direction ? LEFT_sensorOut : RIGHT_sensorOut, LOW);
+  return blueFreq;
+}
+
+const int colorCheckInterval = 50;
+int timer = 0;
+
+
+const int speed = 200;
 
 long duration;
 long distance;
@@ -33,7 +76,25 @@ int numberOfSamples = 10, counter=0;
 
 
 void setup() {
-  // put your setup code here, to run once:
+  // color sensors
+  pinMode(LEFT_S0, OUTPUT);
+  pinMode(LEFT_S1, OUTPUT);
+  pinMode(LEFT_S2, OUTPUT);
+  pinMode(LEFT_S3, OUTPUT);
+  pinMode(LEFT_sensorOut, INPUT);
+  
+  pinMode(RIGHT_S0, OUTPUT);
+  pinMode(RIGHT_S1, OUTPUT);
+  pinMode(RIGHT_S2, OUTPUT);
+  pinMode(RIGHT_S3, OUTPUT);
+  pinMode(RIGHT_sensorOut, INPUT);
+
+  // Setting frequency-scaling to 20%
+  digitalWrite(LEFT_S0, HIGH);
+  digitalWrite(LEFT_S1, HIGH);
+  digitalWrite(RIGHT_S0, HIGH);
+  digitalWrite(RIGHT_S1, HIGH);
+
   pinMode(ena,OUTPUT);
   pinMode(in1,OUTPUT);
   pinMode(in2,OUTPUT);
@@ -85,12 +146,31 @@ void stop() {
 }
 
 void loop() {
-  Serial.print(digitalRead(leftSensor));
-  Serial.print("\t");
-  Serial.print(digitalRead(centerSensor));
-  Serial.print("\t");
-  Serial.println(digitalRead(rightSensor));
+  // Serial.print(digitalRead(leftSensor));
+  // Serial.print("\t");
+  // Serial.print(digitalRead(centerSensor));
+  // Serial.print("\t");
+  // Serial.println(digitalRead(rightSensor));
 
+  if (millis() - timer > colorCheckInterval){
+    Serial.print("Green Left: ");
+    Serial.print(getGreen(LEFT));
+    Serial.print("  Green Right: ");
+    Serial.print(getGreen(RIGHT));
+    Serial.print("   ");
+    Serial.print("Red Left: ");
+    Serial.print(getRed(LEFT));
+    Serial.print("  Red Right: ");
+    Serial.print(getRed(RIGHT));
+    Serial.print("   ");
+    Serial.print("Blue Left: ");
+    Serial.print(getBlue(LEFT));
+    Serial.print("  Blue Right: ");
+    Serial.println(getBlue(RIGHT));
+
+    timer = millis();
+  }
+  
   
   if (digitalRead(leftSensor)==HIGH && digitalRead(centerSensor)==LOW && digitalRead(rightSensor)==HIGH)
   {
